fa5db424b13a4551df1cc4800e6ce3636435bfdf
Eric Hudo <erkinhudo@gmail.com>
2021-06-10 13:59:17 -0400

    Mapping was added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,30 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts"
-import Avatar from "./Avatar"
+
+
+
+function createCard(contact){
+  return(
+    <Card
+    key = {contact.id}
+    id = {contact.id}
+    name = {contact.name}
+    img = {contact.imgURL}
+    tel = {contact.phone}
+    email = {contact.email}
+    />
+  );
+}
+
 
 function App() {
   return (
-    <div >
+    <div>
       <h1 className="heading">My Contacts</h1>
-      
-      <Avatar img = "https://clubhouseprod.s3.amazonaws.com/1396055693_80ec7d19-e13c-454c-bc43-5f78cb6a6f7b"/>
-      <hr/>
-      <Card 
-      name={contacts[0].name}
-      img={contacts[0].imgURL}
-      tel={contacts[0].phone}
-      email={contacts[0].email}
-       />
-
-       <Card 
-       name={contacts[1].name}
-       img={contacts[1].imgURL}
-       tel={contacts[1].phone}
-       email={contacts[1].email}
-        />
-
-        <Card 
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-         />
-
+      {contacts.map(createCard)}
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,23 +2,17 @@ import React from "react";
 import Avatar from "./Avatar"
 import Detail from "./Detail"
 
-
 function Card(props){
-
     return (
       <div className="card">
         <div className="top">
+        <p>{props.id}</p>
           <h2 className="name" >{props.name}</h2>
           <Avatar img={props.img} />
         </div>
         <div className="bottom">
-          <Detail
-          detailInfo = {props.tel}
-          />
-          <Detail
-          detailInfo = {props.email}
-          />
-          
+          <Detail detailInfo = {props.tel} />
+          <Detail detailInfo = {props.tel} />
         </div>
       </div>
     )
